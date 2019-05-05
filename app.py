c765188f76928bbfecb0c25caea0fb087fe21d9b
Harjeet Singh <har.singh@mail.utoronto.ca>
2019-05-05 19:28:11 +0000

    Added complete add and home functionality

--- app.py
+++ app.py
@@ -14,7 +14,21 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    return render_template("recipes.html", 
+            recipes=mongo.db.recipes.find())
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html', 
+            cusine_type=mongo.db.cusine_type.find())
+            
+            
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
 
 
 if __name__ == '__main__':
